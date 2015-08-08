a0c77dd36213ca28bc8b0a892c2649a83ef48544
kirilsagoth <kirilsagoth2@gmail.com>
2015-08-08 22:56:27 +0300

    rabotaet

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
 			},
 			dist: {
 				files: {
-					'html5-boilerplate_v5.2.0/css/scripts.css': 'html5-boilerplate_v5.2.0/sass/scripts.scss'
+					'html5-boilerplate_v5.2.0/css/main.css': 'html5-boilerplate_v5.2.0/sass/scripts.scss'
 				}
 			}
 		},
@@ -21,9 +21,25 @@ module.exports = function(grunt) {
 				files: '**/*.scss',
 				tasks: ['sass']
 			}
+		},
+		wiredep: {
+			task: {
+				// Point to the files that should be updated when
+				// you run `grunt wiredep`
+				src: [
+				  'html5-boilerplate_v5.2.0/index.html'
+				],
+
+				options: {
+				  // See wiredep's configuration documentation for the options
+				  // you may pass:
+
+				  // https://github.com/taptapship/wiredep#configuration
+				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+	grunt.registerTask('default',['sass', 'watch']);
 };
